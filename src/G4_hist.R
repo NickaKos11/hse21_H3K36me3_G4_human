6f33eba85f8be0033e72145577d6b01b06e6542a
NickaKos11 <nicka.costina@gmail.com>
2021-06-09 14:27:01 +0300

    G4_seq_Li_K_hist

---
+++ src/G4_hist.R
@@ -0,0 +1,34 @@
+install.packages("dplyr")
+library(ggplot2)
+library(dplyr)
+# library(tidyr)   # replace_na
+# library(tibble)  # column_to_rownames
+setwd("/Users/kostinanicka/desktop/study/биоинформатика/project/hse21_H3K36me3_G4_human")
+getwd()
+
+###
+
+NAME <- 'G4_seq_Li_K'
+#NAME <- 'H3K36me3_HEK293.ENCFF257ZFX.hg19'
+#NAME <- 'H3K36me3_HEK293.ENCFF257ZFX.hg38'
+#NAME <- 'H3K36me3_HEK293.ENCFF655QDU.hg19'
+#NAME <- 'H3K36me3_HEK293.ENCFF655QDU.hg38'
+OUT_DIR <- 'images/'
+
+###
+
+bed_df <- read.delim(paste0('data/',NAME, '.bed'), as.is = TRUE, header = FALSE)
+
+#colnames(bed_df) <- c('chrom', 'start', 'end', 'name', 'score')
+colnames(bed_df) <- c('chrom', 'start', 'end')
+bed_df$len <- bed_df$end - bed_df$start
+head(bed_df)
+
+# hist(bed_df$len)
+
+ggplot(bed_df) +
+  aes(x = len) +
+  geom_histogram() +
+  ggtitle(NAME, subtitle = sprintf('Number of peaks = %s', nrow(bed_df))) +
+  theme_bw()
+ggsave(paste0('len_hist.', NAME, '.pdf'), path = OUT_DIR)
